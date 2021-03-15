fd2ff76bca2d1fa0cf282a8db938f1c336006ae5
Bruno Farias <begfarias@gmail.com>
2021-03-15 19:37:49 -0300

    :rocket: Challenge completed

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,32 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from '../components/MovieCard';
+
+interface ContentProps {
+  titleGenre: string;
+  movies: Array<{
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }>;
+}
+
+export function Content(props: ContentProps) {
+  return (
+    <>
+      <header>
+        <span className="category">Categoria:<span> {props.titleGenre}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {props.movies.map(movie => (
+            <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </>
+  )
 }
